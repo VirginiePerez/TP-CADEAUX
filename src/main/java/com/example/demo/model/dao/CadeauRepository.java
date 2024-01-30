508a85a819912d6fcccb1b8e526a78937ba78cad
Virginie Perez <p.virgini@gmail.com>
2024-01-30 14:27:22 +0100

    Test Repository JUnit

--- src/main/java/com/example/demo/model/dao/CadeauRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model.dao;
-
-import com.example.demo.model.Cadeau;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CadeauRepository extends JpaRepository<Cadeau, Integer> {
-}
